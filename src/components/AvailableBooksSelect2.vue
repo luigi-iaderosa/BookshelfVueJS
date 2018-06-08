9fa7b764ee277221c6b5144b8803731c1e86a1bc
Luigi Iaderosa <luigi.iaderosa@gmail.com>
2018-06-08 19:03:25 +0200

    add books by multiple component

--- src/components/AvailableBooksSelect2.vue
+++ src/components/AvailableBooksSelect2.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <select v-model="selected">
+    <select v-for="i in inputDimension" v-model="selected[i]">
         <option  v-for="option in options" :value="option.id" > {{option.title}} by {{option.author.complete_name}} </option>
     </select>
-    <button @click="addByButton($event)">Add this!</button>
+    <button @click="enlargeInput">Add more!</button>
+    <button @click="addByButton($event)">Add these!</button>
+
   </div>
 </template>
 
@@ -14,23 +16,26 @@
       props : ['options'],
       data : function() {
         return {
-          selected : ''
+          selected : [],
+          inputDimension: 1
         }
       },
       methods : {
-      addByButton($event){
+        enlargeInput (){
+          this.inputDimension++;
+        },
+        addByButton($event) {
+          for (var x in this.selected){
+            var body = {'id_book': this.selected[x], 'id_user': this.$store.state.user_id};
+            var headers = {headers: {'apitoken': this.$store.state.apitoken}};
+            this.$http.post('http://localhost:8000/api/bookshelf/book', body, headers);
+          }
+          setTimeout(function(){
+            eventBus.$emit('bookAdded')
+          },3000);
 
-
-        var body = {'id_book':this.selected,'id_user':this.$store.state.user_id};
-        var headers = {headers: {'apitoken': this.$store.state.apitoken}};
-
-        this.$http.post('http://localhost:8000/api/bookshelf/book', body,headers);
-        setTimeout(function(){
-          eventBus.$emit('bookAdded')
-        },1000);
-
+          }
         }
-      }
     }
 </script>
 
