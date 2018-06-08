4a513e78c9333051c7ab5dc45fd70001d9fe7f52
Luigi Iaderosa <luigi.iaderosa@gmail.com>
2018-06-08 16:41:10 +0200

    fixing commit (missing files)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,6 +6,11 @@
     <input type="text"  placeholder="Your username" v-model="username"/>
     <input type="password"  placeholder="your password" v-model="password"/>
     <input type="submit" value="Send" @click="submitForm($event)"/>
+    <ul v-if="validationErrors.length" style="background-color: orange">
+      <li style = "color: red " v-for="error in validationErrors">
+        {{error}}
+      </li>
+    </ul>
   </form>
   </div>
 </template>
@@ -17,29 +22,40 @@
     data : function(){
       return {
         username : '',
-        password : ''
+        password : '',
+        validationErrors : []
       }
     },
     methods : {
+      validateSignupForm (){
+        if (this.username == '')
+          this.validationErrors.push('username field has not been provided');
+        if (this.password == ''){
+          this.validationErrors.push('password field has not been provided');
+        }
+
+      },
       submitForm($event){
         $event.preventDefault();
+        this.validationErrors = [];
+        this.validateSignupForm();
 
-        this.$http.post('http://localhost:8000/api/login',{
-          'username':this.username,
-          'password':this.password
-        })
-          .then(response => {
-            return response.json(); //response.json returns a Premise Object (only on first then)
-          }, error => {
-            // console.log(error);
-            // this.$emit('loginUnSuccessful',error.json());
-          }).then(response =>{
-          this.$store.commit('setApitoken',response.apitoken);
-          this.$store.commit('setUserId',response.user_id);
-          this.$store.commit('setBookshelfId',response.bookshelf_id);
-          // console.log(this.$store.state); qui lo stato risulta cambiato
-          this.$emit('loginSuccessful');
-        });
+        if (this.validationErrors.length == 0){
+          this.$http.post('http://localhost:8000/api/login',{
+            'username':this.username,
+            'password':this.password
+          })
+            .then(response => {
+              return response.json(); //response.json returns a Premise Object (only on first then)
+            }, error => {
+            }).then(response =>{
+            this.$store.commit('setApitoken',response.apitoken);
+            this.$store.commit('setUserId',response.user_id);
+            this.$store.commit('setBookshelfId',response.bookshelf_id);
+            this.$emit('loginSuccessful');
+          });
+        }
+
       }
     }
   }
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,10 +2,18 @@
   <div class="form-style-6">
     <h4>Sign up</h4>
     <form>
-      <input type="text"  placeholder="Your Name" v-model="name"/>
+
+      <input type="text"  placeholder="Your Name" v-model="name" />
       <input type="password"  placeholder="your password" v-model="password"/>
       <input type="text"  placeholder="Your username" v-model="username"/>
       <input type="submit" value="Send" @click="submitForm($event)"/>
+      <ul v-if="validationErrors.length" style="background-color: orange">
+          <li style = "color: red " v-for="error in validationErrors">
+            {{error}}
+          </li>
+      </ul>
+
+
     </form>
   </div>
 </template>
@@ -17,28 +25,42 @@
         return {
           username : '',
           name:'',
-          password:''
+          password:'',
+          validationErrors : []
         }
       },
       methods: {
+        validateSignupForm (){
+          if (this.username == '')
+            this.validationErrors.push('username field has not been provided');
+          if (this.name == '')
+            this.validationErrors.push('name field has not been provided');
+          if (this.password == ''){
+            this.validationErrors.push('password field has not been provided');
+          }
+
+        },
         submitForm($event){
           $event.preventDefault();
+          this.validationErrors = [];
+          this.validateSignupForm();
+          if (this.validationErrors.length == 0){
+            this.$http.post('http://localhost:8000/api/signup',{'username':this.username,'name':this.name,'password':this.password})
+              .then(response => {
+                return response.json(); //response.json returns a Premise Object (only on first then)
+            }, error => {
+                console.log(error);
+                this.$emit('signupUnSuccessful',error.json());
+            }).then(response =>{
 
-          this.$http.post('http://localhost:8000/api/signup',{'username':this.username,'name':this.name,'password':this.password})
-            .then(response => {
-              return response.json(); //response.json returns a Premise Object (only on first then)
-          }, error => {
-              console.log(error);
-              this.$emit('signupUnSuccessful',error.json());
-          }).then(response =>{
+              this.$emit('signupSuccessful',response);
+            }).then(data=>{
+                this.$store.commit('setUsername',this.username);
+                this.$store.commit('setName',this.name);
+                this.$store.commit('setPassword',this.password);
+            });
+          }
 
-            this.$emit('signupSuccessful',response);
-          }).then(data=>{
-              this.$store.commit('setUsername',this.username);
-              this.$store.commit('setName',this.name);
-              this.$store.commit('setPassword',this.password);
-
-          });
         }
       }
     }
